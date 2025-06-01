38fcca7e693ab1469cf7d2f8b46a99c0e9c5c227
agunghandiko7 <agunghandiko30@gmail.com>
2025-06-01 23:08:06 +0700

    Add files via upload

---
+++ card.tsx
@@ -0,0 +1,2 @@
+export function Card({ children }) { return <div className="border rounded-xl bg-white shadow">{children}</div>; }
+export function CardContent({ children }) { return <div className="p-4">{children}</div>; }
